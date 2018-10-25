83ac2c5b883f5b97172d79b43df69b437c2ca859
Vladimir Komendantskiy <komendantsky@gmail.com>
2018-10-25 11:39:04 +0100

    mapped the QHB SenderQueueFunc impl to the DHB impl

--- src/queueing_honey_badger/sender_queue.rs
+++ src/queueing_honey_badger/sender_queue.rs
@@ -1,12 +1,10 @@
-use std::cmp;
-
 use rand::Rand;
 use serde::{Deserialize, Serialize};
 
-use super::{Batch, Epoch, Message, QueueingHoneyBadger, Step};
+use super::{Batch, DynamicHoneyBadger, Epoch, Message, QueueingHoneyBadger, Step};
 use sender_queue::SenderQueueFunc;
 use transaction_queue::TransactionQueue;
-use {Contribution, Epoched, NodeIdT};
+use {Contribution, NodeIdT};
 
 impl<T, N, Q> SenderQueueFunc<QueueingHoneyBadger<T, N, Q>> for QueueingHoneyBadger<T, N, Q>
 where
@@ -17,26 +15,19 @@ where
     type Step = Step<T, N, Q>;
 
     fn max_epoch_with_batch(&self, epoch: Epoch, batch: &Batch<T, N>) -> Epoch {
-        cmp::max(*batch.next_epoch(), epoch)
+        self.dyn_hb.max_epoch_with_batch(epoch, batch)
     }
 
-    fn is_accepting_epoch(&self, us: &Message<N>, Epoch((them_era, them_hb_epoch)): Epoch) -> bool {
-        let Epoch((era, hb_epoch)) = us.epoch();
-        era == them_era
-            && (them_hb_epoch <= hb_epoch
-                && hb_epoch <= them_hb_epoch.map(|e| e + self.dyn_hb.max_future_epochs() as u64))
+    fn is_accepting_epoch(&self, us: &Message<N>, them: Epoch) -> bool {
+        self.dyn_hb.is_accepting_epoch(us, them)
     }
 
-    fn is_later_epoch(&self, us: &Message<N>, Epoch((them_era, them_hb_epoch)): Epoch) -> bool {
-        let Epoch((era, hb_epoch)) = us.epoch();
-        era < them_era || (era == them_era && hb_epoch.is_some() && hb_epoch < them_hb_epoch)
+    fn is_later_epoch(&self, us: &Message<N>, them: Epoch) -> bool {
+        self.dyn_hb.is_later_epoch(us, them)
     }
 
     fn spanning_epochs(epoch: Epoch) -> Vec<Epoch> {
-        if let Epoch((era, Some(_))) = epoch {
-            vec![Epoch((era, None))]
-        } else {
-            vec![]
-        }
+        <DynamicHoneyBadger<Vec<T>, N> as SenderQueueFunc<DynamicHoneyBadger<Vec<T>, N>>>
+            ::spanning_epochs(epoch)
     }
 }
